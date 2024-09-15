d386d1cfab9ceadecfbb045758598fcce82992f2
cwto <ctwo@outlook>
2024-09-15 17:27:11 +0800

    git first commit

--- src/main/java/com/example/gitTest/GitTest.java
+++ src/main/java/com/example/gitTest/GitTest.java
@@ -3,5 +3,6 @@ package com.example.gitTest;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git 第一次测试");
+        System.out.println("git 第二次测试");
     }
 }
